c0d85e54306fb3ddf37c57f578b950abc826e877
Lars Boegild Thomsen <lth@netcompartner.com>
2024-01-08 11:20:46 +0800

    Hmmmm

--- prgstl_dfu_bootloader/Core/Src/main.c
+++ prgstl_dfu_bootloader/Core/Src/main.c
@@ -6,7 +6,7 @@
   ******************************************************************************
   * @attention
   *
-  * Copyright (c) 2023 Lars Boegild Thomsen <lbthomsen@gmail.com>
+  * Copyright (c) 2023 Lars Boegild Thomsen <lth@stm32world.com>
   * All rights reserved.
   *
   * This software is licensed under terms that can be found in the LICENSE file
